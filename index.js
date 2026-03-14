92570a0fa924bf5fdfa7cc55dca3899aee2c5f82
Shivani Panchmatia <shivaani8@gmail.com>
2026-03-14 10:42:50 -0600

    Add files via upload
    final ones

--- index.js
+++ index.js
@@ -4,3 +4,15 @@ greeting.style.marginTop = "50px";
 greeting.style.fontSize = "30px";
 const username = window.prompt("What's your name?", "Shivani");
 greeting.innerHTML = "Welcome to my Website, " + username + "!";
+
+// Typewriter effect
+var text = "Shivani Panchmatia";
+var i = 0;
+function typeWriter() {
+    if (i < text.length) {
+        document.getElementById("typewriter").innerHTML += text.charAt(i);
+        i++;
+        setTimeout(typeWriter, 100);
+    }
+}
+typeWriter();
